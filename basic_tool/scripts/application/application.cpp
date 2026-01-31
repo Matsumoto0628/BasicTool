c3f43042341e84a8c5dd0576a6fdcb28bce490c7
Matsumoto0628 <kanikani0628@gmail.com>
2026-01-31 21:41:32 +0900

    feat:アプリケーションクラスの実装

---
+++ basic_tool/scripts/application/application.cpp
@@ -0,0 +1,53 @@
+﻿#include "application.h"
+
+void Application::Initialize(HINSTANCE hInst)
+{
+    m_window.Initialize(hInst);
+}
+
+void Application::Start()
+{
+}
+
+void Application::Loop()
+{
+    while (true) 
+    {
+        // Windowsのメッセージループ
+        {
+            bool result = m_window.MessageLoop();
+            if (result == false) // 後から処理を追加しやすいので、改行する
+            {
+                break;
+            }
+        }
+
+        // ゲームの更新、描画
+        {
+            bool result = gameLoop();
+            if (result == false)
+            {
+                break;
+            }
+        }
+    }
+}
+
+void Application::Terminate()
+{
+}
+
+void Application::Finalize() 
+{
+    m_window.Finalize();
+}
+
+bool Application::gameLoop()
+{
+    // TODO: 更新処理
+    // TODO: 描画処理
+    // TODO: バックバッファ入れ替え
+
+    Sleep(10);
+    return true;
+}
---
+++ basic_tool/scripts/application/application.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "window.h"
+
+class Application
+{
+public:
+    void Initialize(HINSTANCE hInst); // 初期化
+    void Start(); // 開始
+    void Loop(); // 更新、描画
+    void Terminate(); // 終了
+    void Finalize(); // 破棄
+
+private:
+    bool gameLoop();
+
+private:
+    Window m_window;
+};
