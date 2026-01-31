817be99cdf8a1d2a7611172c56e8202e49d6bbc8
Matsumoto0628 <kanikani0628@gmail.com>
2026-01-31 21:25:07 +0900

    feat:ウィンドウクラスの実装

---
+++ basic_tool/scripts/window/window.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <Windows.h>
+
+class Window 
+{
+public:
+	Window(); // 必要に応じてexplicitをつける
+	~Window(); // 必要に応じてvirtualをつける
+	bool Initialize(HINSTANCE hInst);
+	void Finalize();
+	bool MessageLoop();
+	static LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	const HWND GetWindowHandle() const { return m_hWindow; }
+
+private:
+	WNDCLASS m_wc;
+	HWND m_hWindow = nullptr;
+	SIZE m_sizeWindow;
+	MSG m_msg = {};
+
+	// 定数
+	// インスタンス間で1つなので、staticにして宣言のみ行う
+	// 整数型以外や古いコンパイラの場合は定義をソースで行う
+	// 配列の数で使うのであれば、enumハックを使う
+	static const int m_width = 960;
+	static const int m_height = 540;
+};
