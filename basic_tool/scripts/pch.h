3fa8ef0ad351d1ef347682afd06c8b87c2079d35
Matsumoto0628 <kanikani0628@gmail.com>
2026-02-01 01:12:27 +0900

    add:ComPtrを使用可能にする

--- basic_tool/scripts/pch.h
+++ basic_tool/scripts/pch.h
@@ -1,5 +1,10 @@
 ﻿#pragma once
+
+// DirectX
 #include <d3d11.h>
 #include <d3dcompiler.h>
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dcompiler.lib")
+
+// ComPtr
+#include <wrl/client.h>
