9d589a573641bc56f9b9ead6e8aee2d4695dc8e1
Matsumoto0628 <kanikani0628@gmail.com>
2026-02-06 20:38:34 +0900

    change:RenderableをRendererに変更

--- basic_tool/scripts/scene/test_scene.cpp
+++ basic_tool/scripts/scene/test_scene.cpp
@@ -19,56 +19,56 @@ TestScene::~TestScene()
 
 void TestScene::Initialize()
 {
-	std::unique_ptr<Renderable> pSquare = std::make_unique<Square>(m_pContext, m_pCamera);
+	std::unique_ptr<Renderer> pSquare = std::make_unique<Square>(m_pContext, m_pCamera);
 	pSquare->Initialize();
 
-	m_pRenderables.push_back(std::move(pSquare));
+	m_pRenderers.push_back(std::move(pSquare));
 }
 
 void TestScene::Start()
 {
-	for (auto& pRenderable : m_pRenderables) 
+	for (auto& pRenderer : m_pRenderers) 
 	{
-		pRenderable->Start();
+		pRenderer->Start();
 	}
 
-	Transform* pTransform = &(m_pRenderables[0]->GetTransform());
+	Transform* pTransform = &(m_pRenderers[0]->GetTransform());
 	pTransform->SetPosition({0,0,10});
 }
 
 void TestScene::Update()
 {
-	for (auto& pRenderable : m_pRenderables)
+	for (auto& pRenderer : m_pRenderers)
 	{
-		pRenderable->Update();
+		pRenderer->Update();
 	}
 
 	static float time = 0;
 	time += 0.017f;
-	Transform* pTransform = &(m_pRenderables[0]->GetTransform());
+	Transform* pTransform = &(m_pRenderers[0]->GetTransform());
 	pTransform->SetRotation(EulerToQuaternion({ 0,time,0 }));
 }
 
 void TestScene::Draw()
 {
-	for (auto& pRenderable : m_pRenderables)
+	for (auto& pRenderer : m_pRenderers)
 	{
-		pRenderable->Draw();
+		pRenderer->Draw();
 	}
 }
 
 void TestScene::Terminate()
 {
-	for (auto& pRenderable : m_pRenderables)
+	for (auto& pRenderer : m_pRenderers)
 	{
-		pRenderable->Terminate();
+		pRenderer->Terminate();
 	}
 }
 
 void TestScene::Finalize()
 {
-	for (auto& pRenderable : m_pRenderables)
+	for (auto& pRenderer : m_pRenderers)
 	{
-		pRenderable->Finalize();
+		pRenderer->Finalize();
 	}
 }
