b2805e6d5b0bf1d8aef533cc25507251e71585d7
Matsumoto0628 <kanikani0628@gmail.com>
2026-02-06 14:44:36 +0900

    fix:オイラー変換関数の戻り値のconstを外す

--- basic_tool/scripts/math/rotation_converter.h
+++ basic_tool/scripts/math/rotation_converter.h
@@ -2,6 +2,6 @@
 
 float DegToRad(float deg);
 float RadToDeg(float rad);
-const Vec4& EulerToQuaternion(const Vec3& euler);
-const Vec4& AxisToQuaternion(const Vec3& axis, float rad);
+Vec4 EulerToQuaternion(const Vec3& euler);
+Vec4 AxisToQuaternion(const Vec3& axis, float rad);
 extern const float PI; // グローバルを宣言だけ書くときはextern
