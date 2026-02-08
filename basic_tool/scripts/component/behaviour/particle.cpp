8f94e0b624f106d327e91f3d3888efcc5c16f7a5
Matsumoto0628 <kanikani0628@gmail.com>
2026-02-08 23:27:48 +0900

    feat:ビルボードを実装

--- basic_tool/scripts/component/behaviour/particle.cpp
+++ basic_tool/scripts/component/behaviour/particle.cpp
@@ -3,10 +3,11 @@
 #include "rigidbody.h"
 #include "line.h"
 
-Particle::Particle(Transform* pTransform, Rigidbody* pRb, Line* pLine)
+Particle::Particle(Transform* pTransform, Rigidbody* pRb, Line* pLine, Transform* pCameraTransform)
 	: m_pTransform(pTransform),
 	m_pRb(pRb),
-	m_pLine(pLine)
+	m_pLine(pLine),
+	m_pCameraTransform(pCameraTransform)
 {
 }
 
@@ -25,8 +26,16 @@ void Particle::Start()
 void Particle::Update()
 {
 	// デバッグ表示
-	Vec3 start = m_pTransform->GetPosition();
-	m_pLine->SetLine(start, start + m_pRb->GetVelocity().Normalize());
+	{
+		Vec3 start = m_pTransform->GetPosition();
+		m_pLine->SetLine(start, start + m_pRb->GetVelocity().Normalize());
+	}
+
+	// ビルボード
+	{
+		Vec3 dir = (m_pCameraTransform->GetPosition() - m_pTransform->GetPosition()).Normalize();
+		m_pTransform->SetRotation(LookRotation(dir, {0,1,0}));
+	}
 }
 
 void Particle::Terminate()
