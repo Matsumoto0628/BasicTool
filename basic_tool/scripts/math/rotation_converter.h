593465cd6cda9a3dabc2b98d2c316002b8d7b828
Matsumoto0628 <kanikani0628@gmail.com>
2026-02-20 02:10:39 +0900

    feat:クォータニオンからEulerへの変換関数を実装

--- basic_tool/scripts/math/rotation_converter.h
+++ basic_tool/scripts/math/rotation_converter.h
@@ -4,5 +4,6 @@ float DegToRad(float deg);
 float RadToDeg(float rad);
 Vec4 EulerToQuaternion(const Vec3& euler);
 Vec4 AxisToQuaternion(const Vec3& axis, float rad);
+Vec3 QuaternionToEuler(const Vec4& q);
 Vec4 LookRotation(const Vec3& forward, const Vec3& up);
 extern const float PI; // グローバルを宣言だけ書くときはextern
