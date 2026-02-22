4f27032dd8e7830d64a4c5c3ed31f1c00852c6c7
Matsumoto0628 <kanikani0628@gmail.com>
2026-02-22 23:58:30 +0900

    change:InitializeとStartのタイミング見直し

--- basic_tool/scripts/application/scene/test_scene.cpp
+++ basic_tool/scripts/application/scene/test_scene.cpp
@@ -24,41 +24,38 @@ void TestScene::Initialize()
 {
 	m_pGui = std::make_unique<Gui>(m_hWnd, m_pContext, &m_pGameObjects);
 	m_pGui->Initialize();
+	m_pGui->Start();
+}
 
+void TestScene::Start()
+{
 	std::unique_ptr<GameObject> pCameraGameObject = std::make_unique<GameObject>("Camera");
 	auto& camera = pCameraGameObject->AddComponent<Camera>(&pCameraGameObject->GetTransform(), m_pContext->GetWidth(), m_pContext->GetHeight());
 	//pCameraGameObject->AddComponent<CameraController>(&pCameraGameObject->GetTransform());
 	pCameraGameObject->GetTransform().SetPosition({ 0,0,-10 });
 	pCameraGameObject->Initialize();
-	
+	pCameraGameObject->Start();
+
 	for (int i = 0; i < 100; i++)
 	{
 		std::unique_ptr<GameObject> pSpriteGameObject = std::make_unique<GameObject>("Particle");
 		pSpriteGameObject->GetTransform().SetScale({ 0.1f,0.1f,0.1f });
-		pSpriteGameObject->AddComponent<Sprite>(m_pContext, &camera, &pSpriteGameObject->GetTransform(), Vec4{1.5,1.5,1.5,1});
+		pSpriteGameObject->AddComponent<Sprite>(m_pContext, &camera, &pSpriteGameObject->GetTransform(), Vec4{ 1.5,1.5,1.5,1 });
 		auto& line = pSpriteGameObject->AddComponent<Line>(m_pContext, &camera, Vec4{ 1,0,0,1 });
 		auto& rb = pSpriteGameObject->AddComponent<Rigidbody>(&pSpriteGameObject->GetTransform());
-		rb.AddForce({ 
+		rb.AddForce({
 			GameRandom::GetRange(-1.0f, 1.0f),
 			GameRandom::GetRange(-1.0f, 1.0f),
 			GameRandom::GetRange(-1.0f, 1.0f)
-		});
+			});
 		m_pRigidbodies.push_back(&rb);
 		pSpriteGameObject->AddComponent<Particle>(&pSpriteGameObject->GetTransform(), &rb, &line, &pCameraGameObject->GetTransform());
 		pSpriteGameObject->Initialize();
+		pSpriteGameObject->Start();
 		m_pGameObjects.push_back(std::move(pSpriteGameObject));
 	}
 
 	m_pGameObjects.push_back(std::move(pCameraGameObject));
-}
-
-void TestScene::Start()
-{
-	m_pGui->Start();
-	for (auto& pGameObject : m_pGameObjects)
-	{
-		pGameObject->Start();
-	}
 }
 
 void TestScene::Update()
@@ -74,6 +71,8 @@ void TestScene::Update()
 	}
 
 	m_pGui->Update();
+
+	destroy();
 }
 
 void TestScene::Terminate()
