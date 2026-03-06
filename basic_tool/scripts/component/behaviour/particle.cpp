81513c10fab2f91e17806b0c13e15af7fb9fbd3a
Matsumoto0628 <kanikani0628@gmail.com>
2026-03-06 21:48:28 +0900

    feat:寿命とフェードを実装、線の描画を関数に分離

--- basic_tool/scripts/component/behaviour/particle.cpp
+++ basic_tool/scripts/component/behaviour/particle.cpp
@@ -5,18 +5,23 @@
 #include "game_random.h"
 #include "game_object.h"
 #include "scene_manager.h"
+#include "game_time.h"
+#include "sprite.h"
 
-Particle::Particle(uint64_t id, Transform* pTransform, Rigidbody* pRb, Line* pLine, Transform* pCameraTransform)
+Particle::Particle(uint64_t id, Transform* pTransform, Rigidbody* pRb, Line* pLine, Transform* pCameraTransform, Sprite* pSprite, float lifeTime)
 	: Component{ id, Type::Particle },
 	m_pTransform{ pTransform },
 	m_pRb{ pRb },
 	m_pLine{ pLine },
-	m_pCameraTransform{ pCameraTransform }
+	m_pCameraTransform{ pCameraTransform },
+	m_pSprite{pSprite},
+	m_lifeTime{ lifeTime },
+	m_color{m_pSprite->GetColor()}
 {
 }
 
-Particle::Particle(Transform* pTransform, Rigidbody* pRb, Line* pLine, Transform* pCameraTransform)
-	: Particle{ GameRandom::GetUUID(), pTransform, pRb, pLine, pCameraTransform }
+Particle::Particle(Transform* pTransform, Rigidbody* pRb, Line* pLine, Transform* pCameraTransform, Sprite* pSprite, float lifeTime)
+	: Particle{ GameRandom::GetUUID(), pTransform, pRb, pLine, pCameraTransform, pSprite, lifeTime }
 {
 }
 
@@ -35,28 +40,21 @@ void Particle::Start()
 void Particle::Update()
 {
 	// デバッグ表示
-	{
-		const Vec3 worldVel = m_pRb->GetVelocity();
-		Vec3 localVel = worldVel;
-		const auto* const parent = m_pTransform->GetParent();
-
-		if (parent)
-		{
-			if (parent->GetRotation().Length() != 0) // 回転0のときはワールドの速度ベクトルを表示
-			{
-				localVel = parent->GetRotation().RotateVec3(worldVel);
-			}
-		}
-
-		const Vec3 start = m_pTransform->GetPosition();
-		m_pLine->SetLine(start, start + localVel.Normalize());
-	}
+	drawLine();
 
 	// ビルボード
 	{
 		const Vec3 dir = (m_pCameraTransform->GetPosition() - m_pTransform->GetPosition()).Normalize();
 		m_pTransform->SetRotation(LookRotation(dir, {0,1,0}));
 	}
+
+	m_timer += GameTime::GetDeltaTime();
+	if (m_timer >= m_lifeTime)
+	{
+		m_pSprite->SetEnabled(false);
+	}
+	const Vec4 color = { m_color.X(), m_color.Y(), m_color.Z(), m_color.W() * (1 - (m_timer / m_lifeTime)) };
+	m_pSprite->SetColor(color);
 }
 
 void Particle::Draw()
@@ -78,7 +76,8 @@ Json Particle::Serialize() const
 		{"type", GetType()},
 		{"rigidbody", m_pRb->GetID()},
 		{"line", m_pLine->GetID()},
-		{"camera_game_object", m_pCameraTransform->GetGameObject().GetID()}
+		{"camera_game_object", m_pCameraTransform->GetGameObject().GetID()},
+		{"sprite", m_pSprite->GetID()}
 	};
 }
 
@@ -89,7 +88,38 @@ std::unique_ptr<Particle> Particle::Deserialize(const Json& j, Transform* pTrans
 		pTransform,
 		SceneManager::GetCurrentScene()->FindComponent<Rigidbody>(j.at("rigidbody").get<uint64_t>()),
 		SceneManager::GetCurrentScene()->FindComponent<Line>(j.at("line").get<uint64_t>()),
-		&SceneManager::GetCurrentScene()->FindGameObject(j.at("camera_game_object").get<uint64_t>())->GetTransform()
+		&SceneManager::GetCurrentScene()->FindGameObject(j.at("camera_game_object").get<uint64_t>())->GetTransform(),
+		SceneManager::GetCurrentScene()->FindComponent<Sprite>(j.at("sprite").get<uint64_t>()),
+		3.0f // 仮で置いておく
 	);
 	return pComponent;
+}
+
+void Particle::Restart()
+{
+	m_timer = 0;
+	m_pSprite->SetEnabled(true);
+	m_pSprite->SetColor(m_color);
+}
+
+void Particle::drawLine()
+{
+	m_pLine->SetEnabled(m_isLine);
+	if (m_isLine)
+	{
+		const Vec3 worldVel = m_pRb->GetVelocity();
+		Vec3 localVel = worldVel;
+		const auto* const parent = m_pTransform->GetParent();
+
+		if (parent)
+		{
+			if (parent->GetRotation().Length() != 0) // 回転0のときはワールドの速度ベクトルを表示
+			{
+				localVel = parent->GetRotation().RotateVec3(worldVel);
+			}
+		}
+
+		const Vec3 start = m_pTransform->GetPosition();
+		m_pLine->SetLine(start, start + localVel.Normalize());
+	}
 }
--- basic_tool/scripts/component/behaviour/particle.h
+++ basic_tool/scripts/component/behaviour/particle.h
@@ -4,12 +4,13 @@
 class Transform;
 class Rigidbody;
 class Line;
+class Sprite;
 
 class Particle : public Component
 {
 public:
-	Particle(uint64_t id, Transform* pTransform, Rigidbody* pRb, Line* pLine, Transform* pCameraTransform);
-	Particle(Transform* pTransform, Rigidbody* pRb, Line* pLine, Transform* pCameraTransform);
+	Particle(uint64_t id, Transform* pTransform, Rigidbody* pRb, Line* pLine, Transform* pCameraTransform, Sprite* pSprite, float lifeTime);
+	Particle(Transform* pTransform, Rigidbody* pRb, Line* pLine, Transform* pCameraTransform, Sprite* pSprite, float lifeTime);
 	~Particle();
 	void Initialize() override;
 	void Start() override;
@@ -19,11 +20,20 @@ public:
 	void Show() override;
 	Json Serialize() const override;
 	static std::unique_ptr<Particle> Deserialize(const Json& j, Transform* pTransform);
+	void SetIsLine(bool which) { m_isLine = which; }
+	bool GetIsLine() { return m_isLine; }
+	void Restart();
 
 private:
 	Particle() = delete;
+	void drawLine();
 	Transform* const m_pTransform = nullptr;
 	Rigidbody* const m_pRb = nullptr;
 	Line* const m_pLine = nullptr;
 	const Transform* const m_pCameraTransform = nullptr;
+	bool m_isLine = false;
+	float m_lifeTime = 3.0f;
+	float m_timer = 0;
+	Sprite* const m_pSprite = nullptr;
+	const Vec4 m_color;
 };
